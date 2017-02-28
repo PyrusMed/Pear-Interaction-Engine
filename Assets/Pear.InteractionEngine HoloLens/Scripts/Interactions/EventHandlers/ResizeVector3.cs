7a4c9d2ac7607e51dfb95e586b15de1b38e6288c
drryanjames <rcjames1004@gmail.com>
2017-02-28 15:24:25 -0800

    Update resize  logic

--- Assets/Pear.InteractionEngine HoloLens/Scripts/Interactions/EventHandlers/ResizeVector3.cs
+++ Assets/Pear.InteractionEngine HoloLens/Scripts/Interactions/EventHandlers/ResizeVector3.cs
@@ -10,25 +10,20 @@ namespace Pear.InteractionEngine.Interactions.EventHandlers
     /// </summary>
     public class ResizeVector3 : MonoBehaviour, IGameObjectPropertyEventHandler<Vector3>
     {
-        [Tooltip("Zoom speed")]
-        public float ZoomSpeed = 3f;
+        [Tooltip("Resize speed")]
+        public float ResizeSpeed = 3f;
 
         List<GameObjectProperty<Vector3>> _properties = new List<GameObjectProperty<Vector3>>();
 
         void Update()
         {
-            foreach (GameObjectProperty<Vector3> property in _properties)
+            _properties.ForEach(p =>
             {
-                // Better to resize the anchor element than the original object since there could be other
-                // event handlers applying manipulations
-                Anchor anchor = property.Owner.transform.GetOrAddComponent<ObjectWithAnchor>().AnchorElement;
-                float currentScale = anchor.transform.localScale.x;
-                float scaleAmount = property.Value.magnitude * Time.deltaTime;
-                float newScale = currentScale * (1 + scaleAmount);
-
-                // Apply the new scale
-                anchor.transform.localScale = Vector3.one * newScale;
-            }
+                p.Owner.transform.GetOrAddComponent<ObjectWithAnchor>()
+                    .AnchorElement
+                    .transform
+                    .localScale += Vector3.one * p.Value.magnitude * ResizeSpeed * Time.deltaTime;
+            });
         }
 
         public void RegisterProperty(GameObjectProperty<Vector3> property)
