efe42ce1fc607a3e583c88909e68cf363b74c4b0
drryanjames <rcjames1004@gmail.com>
2017-03-04 12:34:00 -0800

    Give ControllerBehavior<T> an editor
    Sets the controller property by default

--- Assets/Pear.InteractionEngine/Scripts/Controllers/ControllerBehavior.cs
+++ Assets/Pear.InteractionEngine/Scripts/Controllers/ControllerBehavior.cs
@@ -6,7 +6,7 @@ namespace Pear.InteractionEngine.Controllers
 	/// Based class for script that need to use a controller
 	/// </summary>
 	/// <typeparam name="T">Type of controller</typeparam>
-	public class ControllerBehavior<T> : MonoBehaviour where T : Controller
+	public class ControllerBehavior<T> : ControllerBehaviorBase where T : Controller
     {
 		// The controller
         public T Controller;
---
+++ Assets/Pear.InteractionEngine/Scripts/Controllers/Editor/ControllerBehaviorEditor.cs
@@ -0,0 +1,44 @@
+﻿using Pear.InteractionEngine.Utils;
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Pear.InteractionEngine.Controllers {
+	[CustomEditor(typeof(ControllerBehaviorBase), true)]
+	[CanEditMultipleObjects]
+	public class ControllerBehaviorEditor : Editor {
+
+		// The controller property
+		SerializedProperty _controller;
+
+		void OnEnable()
+		{
+			_controller = serializedObject.FindProperty("Controller");
+		}
+
+		/// <summary>
+		/// Attempt to set the controller if it is not set
+		/// </summary>
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+
+			// If a reference to the controller is not set
+			// try to find it on the game object
+			if (_controller.objectReferenceValue == null)
+			{
+				// Get the type of controller by examing the templated argument
+				// pased to the controller behavior
+				Type typeOfController = target.GetType().BaseType.GetGenericArguments()[0];
+
+				// Check to see if this component is on the same gameobject as the Controller component.
+				// If so, set this property by default.
+				_controller.objectReferenceValue = ((Component)target).GetComponent(typeOfController);
+			}
+
+			serializedObject.ApplyModifiedProperties();
+
+			DrawDefaultInspector();
+		}
+	}
+}
