5e6a808d6c11ebcb7c8aa6f5bce3b4aa1540a198
drryanjames <rcjames1004@gmail.com>
2017-03-07 06:03:14 -0800

    Add controller, events and eventhandlers

---
+++ Assets/Pear.InteractionEngine Leap Motion/Scripts/Controllers/LeapMotionController.cs
@@ -0,0 +1,58 @@
+﻿using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pear.InteractionEngine.Controllers
+{
+	public class LeapMotionController : Controller
+	{
+		[Tooltip("The hand that effects physics in the world")]
+		[SerializeField]
+		private RigidHand _physicsHand;
+
+		/// <summary>
+		/// The hand associated with this leap motion controller
+		/// NOTE:
+		///		Each hand will have its own leap motion controller object
+		/// </summary>
+		public HandModel Hand
+		{
+			get;
+			private set;
+		}
+
+		void Awake()
+		{
+			InUse = true;
+		}
+
+		// Hook up events
+		void Start()
+		{
+			Hand = GetComponent<HandModel>();
+
+			// When either controller is enabled activate it in the hierarchy
+			OnStartUsing.AddListener((c) => SetActiveFromEnabledDisabled(true));
+
+			// When either controller is disabled deactivate it in the hierarchy
+			OnStopUsing.AddListener((c) => SetActiveFromEnabledDisabled(false));
+		}
+
+		void OnEnable()
+		{
+			if (!InUse)
+				SetActiveFromEnabledDisabled(false);
+		}
+
+		/// <summary>
+		/// Update whether this hand is enabled or disabled, both logically and visually
+		/// </summary>
+		/// <param name="active">Is the hand active?</param>
+		void SetActiveFromEnabledDisabled(bool active)
+		{
+			gameObject.SetActive(active);
+			_physicsHand.gameObject.SetActive(active);
+		}
+	}
+}
---
+++ Assets/Pear.InteractionEngine Leap Motion/Scripts/Interactions/EventHandlers/LeapRTSMove.cs
@@ -0,0 +1,60 @@
+﻿using Leap.Unity;
+using Pear.InteractionEngine.Properties;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Pear.InteractionEngine.Utils;
+
+namespace Pear.InteractionEngine.Interactions.EventHandlers
+{
+	public class LeapRTSMove : MonoBehaviour, IGameObjectPropertyEventHandler<HandModel>
+	{
+		[Tooltip("Method used to rotate with one hand")]
+		public LeapRTS.RotationMethod OneHandedRotationMethod;
+
+		[Tooltip("Method used to rotate with two hands")]
+		public LeapRTS.RotationMethod TwoHandedRotationMethod;
+
+		// LeapRTS is what actually moves the object. We need to update it to make
+		// sure it's in the correct state
+		private LeapRTS _rts;
+
+		void Start()
+		{
+			_rts = transform.GetOrAddComponent<LeapRTS>();
+			_rts.OneHandedRotationMethod = OneHandedRotationMethod;
+			_rts.TwoHandedRotationMethod = TwoHandedRotationMethod;
+		}
+
+		public void RegisterProperty(GameObjectProperty<HandModel> property)
+		{
+			property.ChangeEvent += GrabChanged;
+		}
+
+		public void UnregisterProperty(GameObjectProperty<HandModel> property)
+		{
+			property.ChangeEvent -= GrabChanged;
+		}
+
+		/// <summary>
+		/// Called when the hand starts or ends grabbing.
+		/// Updates the LeapRTS object, which implements movement
+		/// </summary>
+		/// <param name="oldHandValue">old value</param>
+		/// <param name="newHandValue">new value</param>
+		private void GrabChanged(HandModel oldHandValue, HandModel newHandValue)
+		{
+			// LeapRTS uses the PinchDetector to track movement
+			PinchDetector detector = null;
+			if(newHandValue != null)
+				detector = newHandValue.gameObject.GetComponentInChildren<PinchDetector>();
+
+			bool isLeftHand = (newHandValue ?? oldHandValue).GetLeapHand().IsLeft;
+			if (isLeftHand)
+				_rts.PinchDetectorA = detector;
+			else
+				_rts.PinchDetectorB = detector;
+		}
+	}
+}
