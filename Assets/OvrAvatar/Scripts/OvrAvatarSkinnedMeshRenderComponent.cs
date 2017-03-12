6a6066047844837760ec6151ab1f77f803d88f16
drryanjames <rcjames1004@gmail.com>
2017-03-12 15:49:19 -0700

    Add Oculus Utils and OVR Avatar packages

---
+++ Assets/OvrAvatar/Scripts/OvrAvatarSkinnedMeshRenderComponent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Oculus.Avatar;
+
+public class OvrAvatarSkinnedMeshRenderComponent : OvrAvatarRenderComponent
+{
+    Shader surface;
+    Shader surfaceSelfOccluding;
+    SkinnedMeshRenderer mesh;
+    Transform[] bones;
+
+    internal void Initialize(ovrAvatarRenderPart_SkinnedMeshRender skinnedMeshRender, Shader surface, Shader surfaceSelfOccluding, int thirdPersonLayer, int firstPersonLayer, int sortOrder)
+    {
+        this.surfaceSelfOccluding = surfaceSelfOccluding != null ? surfaceSelfOccluding :  Shader.Find("OvrAvatar/AvatarSurfaceShaderSelfOccluding");
+        this.surface = surface != null ? surface : Shader.Find("OvrAvatar/AvatarSurfaceShader");
+        this.mesh = CreateSkinnedMesh(skinnedMeshRender.meshAssetID, skinnedMeshRender.visibilityMask, thirdPersonLayer, firstPersonLayer, sortOrder);
+        this.bones = mesh.bones;
+        UpdateMeshMaterial(skinnedMeshRender.visibilityMask);
+    }
+
+    internal void UpdateSkinnedMeshRender(OvrAvatar avatar, IntPtr renderPart)
+    {
+        ovrAvatarMaterialState materialState = CAPI.ovrAvatarSkinnedMeshRender_GetMaterialState(renderPart);
+        ovrAvatarVisibilityFlags visibilityMask = CAPI.ovrAvatarSkinnedMeshRender_GetVisibilityMask(renderPart);
+        ovrAvatarTransform localTransform = CAPI.ovrAvatarSkinnedMeshRender_GetTransform(renderPart);
+        UpdateSkinnedMesh(avatar, mesh, bones, localTransform, visibilityMask, renderPart);
+        UpdateMeshMaterial(visibilityMask);
+        UpdateAvatarMaterial(mesh.sharedMaterial, materialState);
+    }
+
+    private void UpdateMeshMaterial(ovrAvatarVisibilityFlags visibilityMask)
+    {
+        Shader shader = (visibilityMask & ovrAvatarVisibilityFlags.SelfOccluding) != 0 ? surfaceSelfOccluding : surface;
+        if (mesh.sharedMaterial == null || mesh.sharedMaterial.shader != shader)
+        {
+            mesh.sharedMaterial = CreateAvatarMaterial(gameObject.name + "_material", shader);
+        }
+    }
+}
---
+++ Assets/OvrAvatar/Scripts/OvrAvatarSkinnedMeshRenderPBSComponent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Oculus.Avatar;
+
+public class OvrAvatarSkinnedMeshRenderPBSComponent : OvrAvatarRenderComponent {
+    SkinnedMeshRenderer mesh;
+    Transform[] bones;
+
+    internal void Initialize(ovrAvatarRenderPart_SkinnedMeshRenderPBS skinnedMeshRenderPBS, Shader shader, int thirdPersonLayer, int firstPersonLayer, int sortOrder)
+    {
+        if (shader == null)
+        {
+            shader = Shader.Find("OvrAvatar/AvatarSurfaceShaderPBS");
+        }
+        mesh = CreateSkinnedMesh(skinnedMeshRenderPBS.meshAssetID, skinnedMeshRenderPBS.visibilityMask, thirdPersonLayer, firstPersonLayer, sortOrder);
+        mesh.sharedMaterial = CreateAvatarMaterial(gameObject.name + "_material", shader);
+        bones = mesh.bones;
+    }
+
+    internal void UpdateSkinnedMeshRenderPBS(OvrAvatar avatar, IntPtr renderPart)
+    {
+        ovrAvatarVisibilityFlags visibilityMask = CAPI.ovrAvatarSkinnedMeshRenderPBS_GetVisibilityMask(renderPart);
+        ovrAvatarTransform localTransform = CAPI.ovrAvatarSkinnedMeshRenderPBS_GetTransform(renderPart);
+        UpdateSkinnedMesh(avatar, mesh, bones, localTransform, visibilityMask, renderPart);
+
+        Material mat = mesh.sharedMaterial;
+        UInt64 albedoTextureID = CAPI.ovrAvatarSkinnedMeshRenderPBS_GetAlbedoTextureAssetID(renderPart);
+        UInt64 surfaceTextureID = CAPI.ovrAvatarSkinnedMeshRenderPBS_GetSurfaceTextureAssetID(renderPart);
+        mat.SetTexture("_Albedo", GetLoadedTexture(albedoTextureID));
+        mat.SetTexture("_Surface", GetLoadedTexture(surfaceTextureID));
+    }
+}
