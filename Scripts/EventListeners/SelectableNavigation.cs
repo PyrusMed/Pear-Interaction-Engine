5d2e743c54d43c61c9a4aa9489d6b2f762989e23
drryanjames <rcjames1004@gmail.com>
2017-08-01 18:59:50 -0700

    Add menus that can be navigated with interactions

---
+++ Scripts/EventListeners/SelectableNavigation.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pear.InteractionEngine.Controllers;
+using Pear.InteractionEngine.Events;
+using System;
+using UnityEngine.UI;
+
+namespace Pear.InteractionEngine.EventListeners
+{
+	/// <summary>
+	/// Navigates across selectables based on input
+	/// </summary>
+	public class SelectableNavigation : MonoBehaviour, IEventListener<Vector2>
+	{
+		[Tooltip("Moves when the inputted value is above this threshold")]
+		public float MoveThreshold = 0.5f;
+
+		[Tooltip("Waits this long before move can occur again")]
+		public float MoveDelay = 0.5f;
+
+		// Has the move threshold expired?
+		private bool _canMove = true;
+
+		// The currently selected object
+		private Selectable _selected;
+
+		/// <summary>
+		/// Select the given object
+		/// </summary>
+		/// <param name="controller"></param>
+		/// <param name="selectable"></param>
+		public void Select(Controller controller, Selectable selectable)
+		{
+			if (selectable != _selected && selectable != null)
+			{
+				_selected = selectable;
+				_selected.Select();
+			}
+
+			_canMove = false;
+			StartCoroutine(WaitToMove(MoveDelay));
+		}
+
+		/// <summary>
+		/// Select objects based on input
+		/// </summary>
+		/// <param name="args"></param>
+		public void ValueChanged(EventArgs<Vector2> args)
+		{
+			if (_canMove && Mathf.Abs(args.NewValue.x) > MoveThreshold)
+			{
+				if (_selected != null)
+				{
+					Selectable next = args.NewValue.x > 0 ? _selected.FindSelectableOnRight() : _selected.FindSelectableOnLeft();
+					Select(args.Source, next);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Waits a given amount of time before we can move
+		/// </summary>
+		/// <param name="seconds">Number of seconds to wait</param>
+		private IEnumerator WaitToMove(float seconds)
+		{
+			yield return new WaitForSeconds(seconds);
+			_canMove = true;
+		}
+	}
+}
---
+++ Scripts/UI/SelectableMenu.cs
@@ -0,0 +1,63 @@
+﻿using Pear.InteractionEngine.Controllers;
+using Pear.InteractionEngine.EventListeners;
+using Pear.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pear.InteractionEngine.UI
+{
+	/// <summary>
+	/// A menu that opens around a controller
+	/// and uses selectable navigation
+	/// </summary>
+	public class SelectableMenu : MonoBehaviour
+	{
+		[Tooltip("The default selected object")]
+		public Selectable DefaultSelected;
+
+		[Tooltip("Offset from the controller")]
+		public Vector3 PositionOffset;
+
+		[Tooltip("Local rotation around the parent controller")]
+		public Vector3 RotationOffset;
+
+		[Tooltip("The slider selector")]
+		public SelectableNavigation SliderSelector;
+
+		/// <summary>
+		/// Place the menu around the given controller
+		/// </summary>
+		/// <param name="go"></param>
+		public void OpenAround(Controller controller)
+		{
+			Debug.Log("Opening density menu above " + controller.name);
+
+			gameObject.SetActive(true);
+
+			controller.ActiveObject = gameObject;
+
+			// Anchor to the given controller
+			transform.SetParent(controller.transform, false);
+
+			// Place the menu above the controller
+			transform.localPosition = PositionOffset;
+			transform.localEulerAngles = RotationOffset;
+
+			// Select the default element
+			SliderSelector.Select(controller, DefaultSelected);
+		}
+
+		/// <summary>
+		/// Close the menu
+		/// </summary>
+		/// <param name="controller"></param>
+		public void Close(Controller controller)
+		{
+			Debug.Log("Closing density menu");
+
+			controller.ActiveObject = ModelLoader.Instance.LoadedModel;
+
+			gameObject.SetActive(false);
+		}
+	}
+}
