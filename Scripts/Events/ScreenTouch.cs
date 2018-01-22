261deb9293da569b51a0d6f390bb8440d8011622
drryanjames <rcjames1004@gmail.com>
2018-01-21 17:31:54 -0800

    Listen for screen touches

---
+++ Scripts/Events/ScreenTouch.cs
@@ -0,0 +1,25 @@
+﻿using Pear.InteractionEngine.Controllers;
+using Pear.InteractionEngine.Properties;
+using UnityEngine;
+
+namespace Pear.InteractionEngine.Events
+{
+	/// <summary>
+	/// Event for touching the screen
+	/// </summary>
+	public class ScreenTouch : ControllerBehavior<Controller>, IEvent<bool>
+	{
+
+		[Tooltip("Number of touches needed to fire the event")]
+		[Range(1, 4)]
+		public int NumberOfTouches = 1;
+
+		// Stores the event value that's handled by IEventListener classes
+		public Property<bool> Event { get; set; }
+
+		void Update()
+		{
+			Event.Value = Input.touchCount == NumberOfTouches;
+		}
+	}
+}
